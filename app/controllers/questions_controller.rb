d6ffd00716cca799a7fe568e8f3305f5e4eafaee
Vincent Degove <vincent.degove@muage.net>
2018-11-19 13:01:16 +0100

    Implement the stupid coach logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @coach_answer = "Great!"
+    elsif @question.ends_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
